7623987036232e0e7bcefee6ee3fa130383acd8b
Anton Mishanin <antonmishanin@mail.ru>
2022-03-31 11:30:09 +0700

    simple custom lazy without delegated properties

--- app/src/main/java/com/my/myapplication/MainActivity.kt
+++ app/src/main/java/com/my/myapplication/MainActivity.kt
@@ -2,10 +2,44 @@ package com.my.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val value = MyLazy { SomeClass() }
+        findViewById<View>(R.id.text).setOnClickListener {
+            value.getValue()
+        }
     }
 }
+
+class MyLazy<T>(initializer: () -> T) {
+
+    private var initializer: (() -> T)? = initializer
+    private var isInit = false
+    private var value: T? = null
+
+   fun getValue(): T {
+        return if (isInit) {
+            value as T
+        } else {
+            value = initializer!!()
+            initializer = null
+            isInit = true
+            value as T
+        }
+    }
+}
+
+class SomeClass {
+    init {
+        Log.d("EE", "SomeClass() init")
+    }
+}
+
